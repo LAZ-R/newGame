995d6fcca8b17c75e952cdf4f7a170bc40caccf5
LAZ-R <crazydrawer62@gmail.com>
2020-12-10 23:09:57 +0100

    formatting

--- index.py
+++ index.py
@@ -4,44 +4,49 @@ import calculMultiples
 import calculVolume
 import conversionEuros
 
+
 def menuValueIsTrue(condition):
     menuAskMsg = "\t1 : Quizz Animalier\n\t2 : Encodeur Morse\n\t3 : Calcul de Multiples\n\t4 : Volume d'un Parrallélépipède Rectangle\n\t5 : Convertisseur d'Euros\n\n\t0 : Quitter\n\n"
     menuValueErrorMsg = "Merci de n'utiliser que les caractères proposés.\n"
     while True:
         try:
-            val=(input(menuAskMsg))
-            if val=="<3":
+            val = (input(menuAskMsg))
+            if val == "<3":
                 print("<3 U too")
                 pass
             else:
-                val=float(val)
+                val = float(val)
                 if condition(val):
                     return val
         except:
             pass
-        menuAskMsg=menuValueErrorMsg
+        menuAskMsg = menuValueErrorMsg
+
+
 def index():
     print("\n\t*    Menu    *\n")
 
-    menuValue=menuValueIsTrue(lambda x: 0<= x<= 5)
+    menuValue = menuValueIsTrue(lambda x: 0 <= x <= 5)
 
-    if menuValue==1:
+    if menuValue == 1:
         quizzAnimaux.quizzAnimaux()
         index()
 
-    elif menuValue==2:
+    elif menuValue == 2:
         morseEncode.morseEncode()
         index()
-    elif menuValue==3:
+    elif menuValue == 3:
         calculMultiples.calculMultiples()
         index()
-    elif menuValue==4:
+    elif menuValue == 4:
         calculVolume.calculVolume()
         index()
-    elif menuValue==5:
+    elif menuValue == 5:
         conversionEuros.conversionEuros()
         index()
 
-    else :
+    else:
         print("Au Revoir")
+
+
 index()
