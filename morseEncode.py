7e31147df149a438ee6f1d25d3ee25f4d1e24ae6
LAZ-R <crazydrawer62@gmail.com>
2020-12-15 23:55:10 +0100

    test Fork

--- morseEncode.py
+++ morseEncode.py
@@ -1,124 +1,128 @@
 from unidecode import unidecode
+
+
 def morseEncode():
-    space="    "
-    a="• ▬"
-    b="▬ • • •"
-    c="▬ • ▬ •"
-    d="▬ • •"
-    e="•"
-    f="• • ▬ •"
-    g="▬ ▬ •"
-    h="• • • •"
-    i="• •"
-    j="• ▬ ▬ ▬"
-    k="▬ • ▬"
-    l="• ▬ • •"
-    m="▬ ▬"
-    n="▬ •"
-    o="▬ ▬ ▬"
-    p="• ▬ ▬ •"
-    q="▬ ▬ • ▬"
-    r="• ▬ •"
-    s="• • •"
-    t="▬"
-    u="• • ▬"
-    v="• • • ▬"
-    w="• ▬ ▬"
-    x="▬ • • ▬"
-    y="▬ • ▬ ▬"
-    z="▬ ▬ • •"
-    zero="▬ ▬ ▬ ▬ ▬"
-    one="• ▬ ▬ ▬ ▬"
-    two="• • ▬ ▬ ▬"
-    three="• • • ▬ ▬"
-    four="• • • • ▬"
-    five="• • • • •"
-    six="▬ • • • •"
-    seven="▬ ▬ • • •"
-    eight="▬ ▬ ▬ • •"
-    nine="▬ ▬ ▬ ▬ •"
-    multiplier=0
+    # liste en morse
+    space = "    "
+    a = "• ▬"
+    b = "▬ • • •"
+    c = "▬ • ▬ •"
+    d = "▬ • •"
+    e = "•"
+    f = "• • ▬ •"
+    g = "▬ ▬ •"
+    h = "• • • •"
+    i = "• •"
+    j = "• ▬ ▬ ▬"
+    k = "▬ • ▬"
+    l = "• ▬ • •"
+    m = "▬ ▬"
+    n = "▬ •"
+    o = "▬ ▬ ▬"
+    p = "• ▬ ▬ •"
+    q = "▬ ▬ • ▬"
+    r = "• ▬ •"
+    s = "• • •"
+    t = "▬"
+    u = "• • ▬"
+    v = "• • • ▬"
+    w = "• ▬ ▬"
+    x = "▬ • • ▬"
+    y = "▬ • ▬ ▬"
+    z = "▬ ▬ • •"
+    zero = "▬ ▬ ▬ ▬ ▬"
+    one = "• ▬ ▬ ▬ ▬"
+    two = "• • ▬ ▬ ▬"
+    three = "• • • ▬ ▬"
+    four = "• • • • ▬"
+    five = "• • • • •"
+    six = "▬ • • • •"
+    seven = "▬ ▬ • • •"
+    eight = "▬ ▬ ▬ • •"
+    nine = "▬ ▬ ▬ ▬ •"
+    multiplier = 0
     print("\n\t*    Encodeur Morse    *\n")
     print('\tCaractères autorisés: Alphabet Latin & Chiffres\n\t(tout autre caractère amènera à une conversion en "#")\n')
-    finalUserInput=unidecode(input('\tEntrez le texte à encoder:\n\n')).lower()
+    finalUserInput = unidecode(
+        input('\tEntrez le texte à encoder:\n\n')).lower()
     print("\n\tTexte encodé en Morse:\n")
-    strLenght=len(finalUserInput)
-    while multiplier<strLenght:
-        if finalUserInput[multiplier]==" ":
+    strLenght = len(finalUserInput)
+    while multiplier < strLenght:
+        if finalUserInput[multiplier] == " ":
             print(space, end="")
-        elif finalUserInput[multiplier]=="a":
+        elif finalUserInput[multiplier] == "a":
             print(a, end="   ")
-        elif finalUserInput[multiplier]=="b":
+        elif finalUserInput[multiplier] == "b":
             print(b, end="   ")
-        elif finalUserInput[multiplier]=="c":
+        elif finalUserInput[multiplier] == "c":
             print(c, end="   ")
-        elif finalUserInput[multiplier]=="d":
+        elif finalUserInput[multiplier] == "d":
             print(d, end="   ")
-        elif finalUserInput[multiplier]=="e":
+        elif finalUserInput[multiplier] == "e":
             print(e, end="   ")
-        elif finalUserInput[multiplier]=="f":
+        elif finalUserInput[multiplier] == "f":
             print(f, end="   ")
-        elif finalUserInput[multiplier]=="g":
+        elif finalUserInput[multiplier] == "g":
             print(g, end="   ")
-        elif finalUserInput[multiplier]=="h":
+        elif finalUserInput[multiplier] == "h":
             print(h, end="   ")
-        elif finalUserInput[multiplier]=="i":
+        elif finalUserInput[multiplier] == "i":
             print(i, end="   ")
-        elif finalUserInput[multiplier]=="j":
+        elif finalUserInput[multiplier] == "j":
             print(j, end="   ")
-        elif finalUserInput[multiplier]=="k":
+        elif finalUserInput[multiplier] == "k":
             print(k, end="   ")
-        elif finalUserInput[multiplier]=="l":
+        elif finalUserInput[multiplier] == "l":
             print(l, end="   ")
-        elif finalUserInput[multiplier]=="m":
+        elif finalUserInput[multiplier] == "m":
             print(m, end="   ")
-        elif finalUserInput[multiplier]=="n":
+        elif finalUserInput[multiplier] == "n":
             print(n, end="   ")
-        elif finalUserInput[multiplier]=="o":
+        elif finalUserInput[multiplier] == "o":
             print(o, end="   ")
-        elif finalUserInput[multiplier]=="p":
+        elif finalUserInput[multiplier] == "p":
             print(p, end="   ")
-        elif finalUserInput[multiplier]=="q":
+        elif finalUserInput[multiplier] == "q":
             print(q, end="   ")
-        elif finalUserInput[multiplier]=="r":
+        elif finalUserInput[multiplier] == "r":
             print(r, end="   ")
-        elif finalUserInput[multiplier]=="s":
+        elif finalUserInput[multiplier] == "s":
             print(s, end="   ")
-        elif finalUserInput[multiplier]=="t":
+        elif finalUserInput[multiplier] == "t":
             print(t, end="   ")
-        elif finalUserInput[multiplier]=="u":
+        elif finalUserInput[multiplier] == "u":
             print(u, end="   ")
-        elif finalUserInput[multiplier]=="v":
+        elif finalUserInput[multiplier] == "v":
             print(v, end="   ")
-        elif finalUserInput[multiplier]=="w":
+        elif finalUserInput[multiplier] == "w":
             print(w, end="   ")
-        elif finalUserInput[multiplier]=="x":
+        elif finalUserInput[multiplier] == "x":
             print(x, end="   ")
-        elif finalUserInput[multiplier]=="y":
+        elif finalUserInput[multiplier] == "y":
             print(y, end="   ")
-        elif finalUserInput[multiplier]=="z":
+        elif finalUserInput[multiplier] == "z":
             print(z, end="   ")
-        elif finalUserInput[multiplier]=="0":
+        elif finalUserInput[multiplier] == "0":
             print(zero, end="   ")
-        elif finalUserInput[multiplier]=="1":
+        elif finalUserInput[multiplier] == "1":
             print(one, end="   ")
-        elif finalUserInput[multiplier]=="2":
+        elif finalUserInput[multiplier] == "2":
             print(two, end="   ")
-        elif finalUserInput[multiplier]=="3":
+        elif finalUserInput[multiplier] == "3":
             print(three, end="   ")
-        elif finalUserInput[multiplier]=="4":
+        elif finalUserInput[multiplier] == "4":
             print(four, end="   ")
-        elif finalUserInput[multiplier]=="5":
+        elif finalUserInput[multiplier] == "5":
             print(five, end="   ")
-        elif finalUserInput[multiplier]=="6":
+        elif finalUserInput[multiplier] == "6":
             print(six, end="   ")
-        elif finalUserInput[multiplier]=="7":
+        elif finalUserInput[multiplier] == "7":
             print(seven, end="   ")
-        elif finalUserInput[multiplier]=="8":
+        elif finalUserInput[multiplier] == "8":
             print(eight, end="   ")
-        elif finalUserInput[multiplier]=="9":
+        elif finalUserInput[multiplier] == "9":
             print(nine, end="   ")
         else:
             print("#", end="   ")
-        multiplier=multiplier+1
+        multiplier = multiplier+1
     print("\n")
