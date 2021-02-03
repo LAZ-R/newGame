8239d931188d4f03b2ae81f2384f472ee2422f09
LAZ-R <crazydrawer62@gmail.com>
2021-02-03 14:13:29 +0100

    faible optimisation

--- quizzAnimaux.py
+++ quizzAnimaux.py
@@ -1,33 +1,35 @@
-from unidecode import unidecode 
+from unidecode import unidecode
 
-def isAnswerOk (askMsg, trueAnswer):
-    winMsg="\tbravo\n"
-    failMsg="\traté, essayes encore\n\n"
+
+def isAnswerOk(askMsg, trueAnswer):
+    winMsg = "\tbravo\n"
+    failMsg = "\traté, essayes encore\n\n"
     while True:
         try:
-            answer=unidecode(input(askMsg)).lower()
-            if answer==trueAnswer:
+            answer = unidecode(input(askMsg)).lower()
+            if answer == trueAnswer:
                 print(winMsg)
                 return
         except:
             pass
-        askMsg=failMsg
+        askMsg = failMsg
+
 
 def quizzAnimaux():
-    levelNumber=1
-    
+    levelNumber = 1
+
     print("\n\t*    Quizz animalier    *\n\n")
 
     print("\t---- Level", levelNumber, "----\n")
     isAnswerOk("\tGris, trompe\n\n", "elephant")
-    levelNumber=levelNumber+1
+    levelNumber += 1
 
     print("\t---- Level", levelNumber, "----\n")
     isAnswerOk("\tCheval, rayé\n\n", "zebre")
-    levelNumber=levelNumber+1
+    levelNumber += 1
 
     print("\t---- Level", levelNumber, "----\n")
     isAnswerOk("\tBoule, griffes\n\n", "tatou")
-    levelNumber=levelNumber+1
+    levelNumber += 1
 
     print("pour l'instant, c'est tout !")
